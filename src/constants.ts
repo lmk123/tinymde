38a4287ee68ba4a8b53f0fe7c3a234ad857a4fa3
lmk123 <me@limingkai.cn>
2017-08-14 16:09:42 +0800

    使用枚举定义 kdyCode 到数字的映射

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,11 @@
 // 快捷键的修饰符
 export const MODIFIER: string = window.navigator.userAgent.toLowerCase().indexOf('mac') > 0 ? 'metaKey' : 'ctrlKey'
 
-interface KeyMap {
-  [keyCode: string]: number
-}
-
-export const KEY_CODES: KeyMap = {
-  b: 66, // Command + B  粗体
-  h: 72, // Command + H  标题
-  i: 73, // Command + I  斜体
-  u: 85, // Command + U  下划线
-  z: 90 // Command + Z   撤销
+// keyCode 到数字的映射
+export const enum KEYS {
+  B = 66, // Command + B  粗体
+  H = 72, // Command + H  标题
+  I = 73, // Command + I  斜体
+  U = 85, // Command + U  下划线
+  Z = 90 // Command + Z   撤销
 }
