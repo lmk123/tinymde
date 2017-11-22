0d233c0e9e9f8a234d92a4004e796c7a8a5433e6
lmk123 <me@limingkai.cn>
2017-11-22 14:24:56 +0800

    完成 list 类型的恢复操作

--- src/manipulate-state/try-restore.ts
+++ src/manipulate-state/try-restore.ts
@@ -7,10 +7,10 @@ export const restoreFns = {
   strikethrough: tryUnWrap('~~'),
   inlineCode: tryUnWrap('`'),
   blockCode,
-  ul: tryUnlist(/\-\s/),
-  ol: tryUnlist(/\d+\.\s/),
-  quote: tryUnlist(/>\s/),
-  task: tryUnlist(/\-\s\[[\sx]\]\s/)
+  ul: tryUnlist('\\-\\s'),
+  ol: tryUnlist('\\d+\\.\\s'),
+  quote: tryUnlist('>\\s'),
+  task: tryUnlist('\\-\\s\\[[\\sx]\\]\\s')
 }
 
 export type KeyOfRestoreFns = keyof typeof restoreFns
@@ -52,8 +52,9 @@ function blockCode(state: IState) {
   } else {
     wrapEnd += 1
   }
-  if (value.slice(selectionEnd, wrapEnd) !== '\n```' + (endEdge ? '' : '\n'))
+  if (value.slice(selectionEnd, wrapEnd) !== '\n```' + (endEdge ? '' : '\n')) {
     return
+  }
   // 判断选中文本前一个字符是否是 \n
   const prevInedex = selectionStart - 1
   if (value[prevInedex] !== '\n') return
@@ -67,17 +68,48 @@ function blockCode(state: IState) {
   if (
     value.slice(wrapStart, wrapStart + (startEdge ? 3 : 4)) !==
     (startEdge ? '' : '\n') + '```'
-  )
+  ) {
     return
+  }
   const selectedStr = value.slice(selectionStart, selectionEnd)
-  if (!startEdge && value[wrapStart - 1] === '\n') wrapStart -= 1
-  if (value[wrapEnd] === '\n') wrapEnd += 1
+  // 暂时不要去掉额外的换行符
+  // if (!startEdge && value[wrapStart - 1] === '\n') wrapStart -= 1
+  // if (value[wrapEnd] === '\n') wrapEnd += 1
   state.value = stringSplice(value, wrapStart, wrapEnd, selectedStr)
   state.selectionStart = wrapStart
   state.selectionEnd = wrapStart + selectedStr.length
   return true
 }
 
-function tryUnlist(char: RegExp) {
-  return function(state: IState) {}
+function tryUnlist(regStr: string) {
+  return function(state: IState) {
+    const { value, selectionStart, selectionEnd } = state
+    // 先判断选中文本的前面一部分是否满足正则
+    const startIndex = value.lastIndexOf('\n', selectionStart - 1) + 1
+    const beforeStr = value.slice(startIndex, selectionStart)
+    if (createRegExp(`^${regStr}$`).test(beforeStr)) {
+      const { length } = beforeStr
+      state.value = stringSplice(value, startIndex, selectionStart)
+      state.selectionStart = selectionStart - length
+      state.selectionEnd = selectionEnd - length
+      return true
+    }
+    // 判断整个选中文本是否符合 list 条件
+    const selectedStr = value.slice(selectionStart, selectionEnd)
+    const wholeReg = createRegExp(`^(${regStr}[^\\n]*\\n?)+$`)
+    if (!wholeReg.test(selectedStr)) return
+
+    const tripSymbol = selectedStr.replace(createRegExp(regStr, 'g'), '')
+    state.value = stringSplice(value, selectionStart, selectionEnd, tripSymbol)
+    state.selectionStart = selectionStart
+    state.selectionEnd = selectionEnd - (selectedStr.length - tripSymbol.length)
+    return true
+  }
+}
+
+const regExpMap: { [regExpStr: string]: RegExp } = {}
+
+function createRegExp(str: string, flag?: string) {
+  const cacheKey = str + (flag || '')
+  return regExpMap[cacheKey] || (regExpMap[cacheKey] = new RegExp(str, flag))
 }
