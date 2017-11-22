32e086dd1b021dc56024e1312aeded2821993b96
lmk123 <me@limingkai.cn>
2017-11-22 12:32:58 +0800

    完善 blockCode

--- src/manipulate-state/try-restore.ts
+++ src/manipulate-state/try-restore.ts
@@ -25,34 +25,50 @@ function tryUnWrap(char: string) {
     const { length } = char
     const wrapStart = selectionStart - length
     const wrapEnd = selectionEnd + length
-    if (wrapStart >= 0 && wrapEnd <= value.length) {
-      const startChar = value.slice(wrapStart, selectionStart)
-      const endChar = value.slice(selectionEnd, wrapEnd)
-      if (startChar === endChar && startChar === char) {
-        const selectedStr = value.slice(selectionStart, selectionEnd)
-        state.value = stringSplice(value, wrapStart, wrapEnd, selectedStr)
-        state.selectionStart = wrapStart
-        state.selectionEnd = wrapStart + selectedStr.length
-        return true
-      }
-    }
+    if (wrapStart < 0 || wrapEnd > value.length) return
+    const startChar = value.slice(wrapStart, selectionStart)
+    const endChar = value.slice(selectionEnd, wrapEnd)
+    if (startChar !== endChar || startChar !== char) return
+    const selectedStr = value.slice(selectionStart, selectionEnd)
+    state.value = stringSplice(value, wrapStart, wrapEnd, selectedStr)
+    state.selectionStart = wrapStart
+    state.selectionEnd = wrapStart + selectedStr.length
+    return true
   }
 }
 
 function blockCode(state: IState) {
   const { value, selectionStart, selectionEnd } = state
 
-  // 判断选中文本后四个字符是否是 \n```
-  let wrapEnd = selectionEnd + 5
-  if (value.slice(selectionEnd, wrapEnd) !== '\n```\n') return
+  // 判断选中文本后四个字符是否是 \n```。如果没有触到文本结尾，则要多判断一个换行
+  const { length } = value
+  let wrapEnd = selectionEnd + 4
+  if (wrapEnd > length) return
+  let endEdge
+  if (wrapEnd === length) {
+    endEdge = true
+  } else {
+    wrapEnd += 1
+  }
+  if (value.slice(selectionEnd, wrapEnd) !== '\n```' + (endEdge ? '' : '\n'))
+    return
   // 判断选中文本前一个字符是否是 \n
   const prevInedex = selectionStart - 1
   if (value[prevInedex] !== '\n') return
-  // 判断前面的一行是否以 ``` 开头
+  // 判断前面的一行是否以 \n``` 开头。如果触到了文本开头，则不要求有第一个换行符。
   let wrapStart = value.lastIndexOf('\n', prevInedex - 1)
-  if (value.slice(wrapStart, wrapStart + 4) !== '\n```') return
+  let startEdge
+  if (wrapStart < 0) {
+    wrapStart = 0
+    startEdge = true
+  }
+  if (
+    value.slice(wrapStart, wrapStart + (startEdge ? 3 : 4)) !==
+    (startEdge ? '' : '\n') + '```'
+  )
+    return
   const selectedStr = value.slice(selectionStart, selectionEnd)
-  if (value[wrapStart - 1] === '\n') wrapStart -= 1
+  if (!startEdge && value[wrapStart - 1] === '\n') wrapStart -= 1
   if (value[wrapEnd] === '\n') wrapEnd += 1
   state.value = stringSplice(value, wrapStart, wrapEnd, selectedStr)
   state.selectionStart = wrapStart
